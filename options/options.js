98c49cc54e6f53f3a40dd48f0dcb2c3f355560f7
Crowdbotics <team@crowdbotics.com>
2023-09-13 00:41:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "talk-to-me-43364",
+  url: "https://talk-to-me-43364.botics.co",
+  api: "https://talk-to-me-43364.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
